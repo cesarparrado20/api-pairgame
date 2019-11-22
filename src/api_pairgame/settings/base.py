61545d47b12369707e84ced5310b5d3b08a0d624
CÉSAR PARRADO <cesarparrado970@gmail.com>
2019-11-22 10:58:03 -0500

    creación de modelos

--- src/api_pairgame/settings/base.py
+++ src/api_pairgame/settings/base.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -37,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'worlds',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'es'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'America/Bogota'
 
---
+++ src/worlds/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class World(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = "world"
+        verbose_name_plural = "worlds"
+        db_table = "worlds"
+
+    def __str__(self):
+        return "World: {}".format(self.name)
+
+
+class Image(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    url = models.URLField()
+    consultation_data = models.DateTimeField(auto_now_add=True)
+    world = models.ForeignKey(World, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "image"
+        verbose_name_plural = "images"
+        db_table = "images"
+
+    def __str__(self):
+        return "{} world image".format(self.world.name)
